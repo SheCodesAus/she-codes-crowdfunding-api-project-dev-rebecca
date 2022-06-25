4c5722c36ea5a4d504e4f8280cfd1872f08e07ed
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-06-25 12:14:47 +1000

    project and pledges setup

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  image = models.URLField()
+  date_created = models.DateTimeField()
+  artist = models.CharField(max_length=200)
+
+class Pledge(models.Model):
+  amount = models.IntegerField()
+  comment = models.CharField(max_length=200)
+  anonymous = models.BooleanField()
+  project = models.ForeignKey(
+    'Project',
+    on_delete=models.CASCADE,
+    related_name='pledges'
+  )
+  supporter = models.CharField(max_length=200)
